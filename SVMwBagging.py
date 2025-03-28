b2c78ca64fd78d622baa3d6e51dfc05fe350c6bc
OwenH <otharty@gmail.com>
2025-03-28 15:10:08 -0500

    Small updates.

--- SVMwBagging.py
+++ SVMwBagging.py
@@ -47,7 +47,7 @@ class SVMwBagging:
             raise RuntimeError("BaggingSVM must be trained on data before predicting, call train() first")
 
         # Get predictions from each model
-        print(len(self.models))
+        #print(len(self.models))
         predictions = np.array([model.predict(testData) for model in self.models])
 
         # Convert string labels to numerical if needed
